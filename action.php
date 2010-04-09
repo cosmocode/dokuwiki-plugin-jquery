08ebbd9fb42ba2da4e9323a72b102ea5f380d88b
Adrian Lang <lang@cosmocode.de>
2010-04-09 12:53:44 +0200

    Update plugin info and use plugin.info.txt

--- action.php
+++ action.php
@@ -13,13 +13,6 @@ require_once(DOKU_PLUGIN.'action.php');
 
 class action_plugin_jquery extends DokuWiki_Action_Plugin {
 
-    /**
-     * return some info
-     */
-    function getInfo(){
-        return confToHash(dirname(__FILE__).'/INFO.txt');
-    }
-
     /*
      * plugin should use this method to register its handlers with the dokuwiki's event controller
      */
